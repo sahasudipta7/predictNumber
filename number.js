71abd470475c80af97960ec438d6aafa85e2b592
Sudipta Saha <148531317+sahasudipta7@users.noreply.github.com>
2025-07-05 22:46:04 +0530

    project guess number

---
+++ number.js
@@ -0,0 +1,35 @@
+const randomNumber = Math.floor(Math.random()*100+1);
+const result = document.querySelector("#result");
+const form = document.querySelector("form");
+const prevGuess=document.querySelector("#prevGuess");
+const guessRemaining = document.querySelector("#guessRem");
+let guessR=10;
+let playGame = true;
+
+while(playGame){
+    form.addEventListener("submit", (e)=>{
+    e.preventDefault();
+    const userNumber = parseInt(document.querySelector("#number").value);
+    if(userNumber === randomNumber){
+        result.innerHTML="Congratulations,You Win!";
+    }
+    else if(userNumber < randomNumber){
+        result.innerHTML="Too low!";
+        guessR--;
+        guessRemaining.innerHTML=`Guesses Remaining: ${guessR}`; 
+    }
+    else{
+        result.innerHTML="Too high!";
+        guessR--;
+        guessRemaining.innerHTML=`Guesses Remaining: ${guessR}`; 
+    }
+    const pg = document.createTextNode(` ${userNumber},`);
+    prevGuess.appendChild(pg);
+    if(guessR==0){
+        playGame=false;
+        alert("guesses are over");        
+    }
+} )
+
+}
+
